fcaff669de36b03c41de3af9d1be36969f8211fb
Brandon <kiariebrandon56@gmail.com>
2023-10-18 13:21:10 +0300

    Added my qualifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============ACCORDION================*/
 const skillsContent = document.getElementsByClassName('skills__content'),
-      skillsHeader = document.querySelector('.skills__header')
+      skillsHeader = document.querySelectorAll('.skills__header')
 
 function toggleSkills() {
     let itemClass = this.parentNode.className
@@ -41,7 +41,7 @@ function toggleSkills() {
     for(i = 0; i < skillsContent.length; i++){
         skillsContent[i].className = 'skills__content skills__close'
     }
-    if (itemClass === 'skills__content skills__close'){
+    if (itemClass === 'skills__content skills__close')  {
         this.parentNode.className = 'skills__content skills__open'
     }
 }
